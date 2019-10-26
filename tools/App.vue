3bb06c94234bd0c6d8447b010623f94dbe6b8a80
David Konsumer <konsumer@jetboystudio.com>
2019-10-26 01:30:04 -0700

    working basic vue app

---
+++ tools/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id='root'>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Using Parcel In A Vue.js App',
+    };
+  },
+};
+</script>
